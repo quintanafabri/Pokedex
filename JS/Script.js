605c20bb92591ce5278ec7d7888141a456596b04
Fabricio Quintana <88584654+quintanafabri@users.noreply.github.com>
2021-12-13 23:35:07 -0300

    1.0

---
+++ JS/Script.js
@@ -0,0 +1,57 @@
+const Pokedex_URL = "https://quintanafabri.github.io/pokemon-data/pokedex.json" 
+let  PokedexData = {};// Lugar donde guardamos los datos del archivo JSON
+let PokemonID = 0; // Lugar que ocupa en el JSON "Pokedex.json"
+
+
+
+
+
+function numeroRandomMath() {  // Funcion para generar el numero random y asignar ese numero a PokemonID.
+    let numero = Math.round( Math.random() * (807 - 0) + 0);
+    PokemonID = numero;
+    ImagenLoad();
+   
+}
+
+
+function ImagenLoad(){ // Funcion que generara la imagen
+    let imagenHTML = '';
+    let PokemonIDstr = (PokemonID+1).toString(); // pasamos PokemonID a STRING
+
+   const fotoID = PokemonIDstr.padStart(3,"0") // "Rellenamos" para que el numero tenga un formato de 3 digitos
+    imagenHTML = `    <img id="imagen" src="https://quintanafabri.github.io/pokemon-data/images/`+fotoID+`.png" alt="">
+    `
+
+document.getElementById("foto").innerHTML = imagenHTML;
+};
+
+
+function play(pokemon){//Funcion que devuelve la respuesta si ganaste o perdiste
+if (document.getElementById("respuesta").value === PokedexData[PokemonID].name.english){
+alert("GANASTE!!!!!!")}
+else{
+    alert("PERDISTE!")
+}
+}
+
+
+
+
+
+// EVENTOS
+document.addEventListener("DOMContentLoaded", function(e){
+  // FETCH--Request!
+    fetch(Pokedex_URL)
+    .then(res => res.json())
+    .then(data => {
+        PokedexData = data;
+    })
+
+
+    imagenHTML = `    <img id="imagen" src="https://quintanafabri.github.io/pokemon-data/images/810.png" alt="">
+    `
+
+document.getElementById("foto").innerHTML = imagenHTML;
+
+    
+});
